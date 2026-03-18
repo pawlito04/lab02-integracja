bf9e9073d65ff4842393dd7d6920d7dd84c02178
Paweł Kalinowski <25929@edu.amw.gdynia.pl>
2026-03-18 10:56:40 +0100

    Add blog app views and urls

--- blog/urls.py
+++ blog/urls.py
@@ -3,6 +3,6 @@ from . import views
 
 urlpatterns = [
     path('', views.post_list, name='post_list'),
-    path('post/<int:pk>/', views.post_detail, name='post_detail'),  
+    path('post/<int:pk>/', views.post_detail, name='post_detail'),
+    path('api/posts/', views.post_list_api, name='post_list_api'),
 ]
-
--- blog/views.py
+++ blog/views.py
@@ -1,11 +1,28 @@
+from django.http import JsonResponse
 from django.shortcuts import render, get_object_or_404
 from django.utils import timezone
 from .models import Post
 
+
 def post_list(request):
-    posts = Post.objects.filter(published_at__lte=timezone.now()).order_by('-published_at')
+    posts = Post.objects.filter(
+        published_at__lte=timezone.now()
+    ).order_by('-published_at')
     return render(request, 'blog/post_list.html', {'posts': posts})
 
+
 def post_detail(request, pk):
-    post = get_object_or_404(Post, pk=pk, published_at__lte=timezone.now())
+    post = get_object_or_404(
+        Post,
+        pk=pk,
+        published_at__lte=timezone.now()
+    )
     return render(request, 'blog/post_detail.html', {'post': post})
+
+
+def post_list_api(request):
+    posts = Post.objects.filter(
+        published_at__lte=timezone.now()
+    ).values('id', 'title', 'content', 'author__username', 'published_at')
+    data = list(posts)
+    return JsonResponse(data, safe=False)
