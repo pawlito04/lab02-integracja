3de2ef11ea28f298b856106406cee0b091e77d26
Paweł Kalinowski <25929@edu.amw.gdynia.pl>
2026-04-22 11:13:32 +0200

    Add unit tests for the application

--- blog/tests.py
+++ blog/tests.py
@@ -1,3 +1,52 @@
 from django.test import TestCase
+from django.contrib.auth.models import User
+from django.utils import timezone
 
-# Create your tests here.
+from .models import Post
+
+
+class PostModelTest(TestCase):
+    def setUp(self):
+        self.user = User.objects.create_user(
+            username='testuser',
+            password='testpass123'
+        )
+
+    def test_post_string_representation(self):
+        post = Post.objects.create(
+            title='Testowy wpis',
+            content='To jest treść testowego wpisu.',
+            author=self.user,
+            published_at=timezone.now()
+        )
+        self.assertEqual(str(post), 'Testowy wpis')
+
+
+class PostViewsTest(TestCase):
+    def setUp(self):
+        self.user = User.objects.create_user(
+            username='testuser',
+            password='testpass123'
+        )
+        self.post = Post.objects.create(
+            title='Widoczny wpis',
+            content='Treść wpisu',
+            author=self.user,
+            published_at=timezone.now()
+        )
+
+    def test_post_list_view_status_code(self):
+        response = self.client.get('/blog/')
+        self.assertEqual(response.status_code, 200)
+
+    def test_post_list_view_uses_correct_template(self):
+        response = self.client.get('/blog/')
+        self.assertTemplateUsed(response, 'blog/post_list.html')
+
+    def test_post_detail_view_status_code(self):
+        response = self.client.get(f'/blog/post/{self.post.pk}/')
+        self.assertEqual(response.status_code, 200)
+
+    def test_post_detail_view_uses_correct_template(self):
+        response = self.client.get(f'/blog/post/{self.post.pk}/')
+        self.assertTemplateUsed(response, 'blog/post_detail.html')
