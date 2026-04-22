dcfe0c3bd2aa995303ce975270a0094a3a926aca
Paweł Kalinowski <25929@edu.amw.gdynia.pl>
2026-04-22 12:20:50 +0200

    Fix flake8 style issues

--- blog/models.py
+++ blog/models.py
@@ -2,6 +2,7 @@ from django.db import models
 from django.contrib.auth.models import User
 from django.utils import timezone
 
+
 class Post(models.Model):
     title = models.CharField(max_length=200)
     content = models.TextField()
